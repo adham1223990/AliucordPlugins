84489a971cb5c709cecd8ee60947a01fd8f6c67d
adhamgithub <adh01640@gmail.com>
2026-09-21 23:54:02 +0300

    Add build.gradle.kts for BetterStreamQuality plugin
    Initial setup for BetterStreamQuality plugin with version and description.

---
+++ plugins/BetterStreamQuality/build.gradle.kts
@@ -0,0 +1,11 @@
+import com.aliucord.gradle.AliucordExtension
+
+version = "1.0.0"
+
+description = "NitroSpoof, adds highest stream quality without nitro.."
+
+configure<AliucordExtension> {
+    // TODO: replace 0L with your real Discord user ID if you want the
+    // author name to link to your profile in the plugin list
+    author("Adham", 0L, hyperlink = true)
+}
