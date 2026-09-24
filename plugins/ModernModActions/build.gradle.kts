8907a3541ce4ac808da93d6cf8485ee818e2b790
adhamgithub <adh01640@gmail.com>
2026-09-24 12:34:38 +0300

    Add build.gradle.kts for ModernModActions plugin
    Initial release of the ModernModActions plugin with version 1.0.0.

---
+++ plugins/ModernModActions/build.gradle.kts
@@ -0,0 +1,13 @@
+version = "1.0.0"
+description = "Fixes moderation actions"
+
+aliucord {
+    changelog.set(
+        """
+        # 1.0.0
+        * Initial plugin release.
+        """.trimIndent()
+    )
+    author("Adham", 1001222848716738570L, hyperlink = false)
+    deploy.set(true)
+}
