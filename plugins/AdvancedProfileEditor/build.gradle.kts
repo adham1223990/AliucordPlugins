4106a3dfa7861bd69789e86f0762f512b1ca83a8
adhamgithub <adh01640@gmail.com>
2026-09-25 18:21:27 +0300

    Add build.gradle.kts for AdvancedProfileEditor plugin

---
+++ plugins/AdvancedProfileEditor/build.gradle.kts
@@ -0,0 +1,11 @@
+import com.aliucord.gradle.AliucordExtension
+
+version = "1.0.0"
+
+description = "Backport DiscordRN profile editor"
+
+configure<AliucordExtension> {
+    // TODO: replace 0L with your real Discord user ID if you want the
+    // author name to link to your profile in the plugin list
+    author("Adham", 0L, hyperlink = true)
+}
